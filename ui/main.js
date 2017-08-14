c88e88984c6bc0251b28f6b2cad50b74b4287ef1
harichandharsha <30806647+harichandharsha@users.noreply.github.com>
2017-08-14 06:43:15 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,6 +13,33 @@ button.onclick = function(){
             //take some action
             if(request.status===200){
             var names=request.responseText;
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML=counter.toString();
+            }
+        }
+        //not done yet
+    };
+    //make req
+    request.open('GET','http://haricmdk.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
+//submit name
+
+    
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    //make a req to the server and send the name
+    //capture a list of names and render it as a list
+    //create a req obj
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            //take some action
+            if(request.status===200){
+            var names=request.responseText;
             names=JSON.parse(names);
              var list = '';
             for(var i=0; i<names.length;i++){
@@ -26,16 +53,9 @@ button.onclick = function(){
         //not done yet
     };
     //make req
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET','http://haricmdk.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
-};
-//submit name
-
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-submit.onclick = function(){
-    //make a req to the server and send the name
-    //capture a list of names and render it as a list
     
 };
