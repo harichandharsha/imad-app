ee6b62da0d48618d44d49dc563261aaec64deacf
harichandharsha <30806647+harichandharsha@users.noreply.github.com>
2017-08-19 06:26:47 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ function createTemplate(data)
                     <div>
                         ${content}
                     </div>
-                <form method='post' action='http://haricmdk.imad.hasura-app.io/comments'>
+                <form method='post' action='/comments'>
                 <hr/>
                   <br />
                   <textarea name='comment' id='comments' cols="70" rows="10"></textarea><br />
