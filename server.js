7e1a1b8fc2dd59a46b60c7bd6c558dd8450ce4ef
harichandharsha <30806647+harichandharsha@users.noreply.github.com>
2017-08-19 06:56:37 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ var comments=[];
 app.get('/comments',function(req,res){//url:/submit-name?home=xxxx
    //get the name from the request
    var comments=req.query.comments;
-   comments.push(comments);
+ names.push(comments);
    //JSON:JavaScript Object Notation
    res.send(JSON.stringify(comments));
 });
