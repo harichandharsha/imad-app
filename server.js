df21fdb3aa4a354abf9248c2713d0b56e8532120
harichandharsha <30806647+harichandharsha@users.noreply.github.com>
2017-08-25 05:46:19 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,10 +80,12 @@ app.post('/create-user',function(req,res){
      if(err){
             res.status(500).send(err.toString());
         }else{
-            res.send(JSON.stringify(result.rows));
+            res.send('user created successfully:'+username);
         }  
    });
 });
+
+
 
 
 var pool = new Pool(config);
