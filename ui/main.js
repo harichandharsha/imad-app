08583565a3da89056663cff79932f477ff43b697
harichandharsha <30806647+harichandharsha@users.noreply.github.com>
2017-08-25 11:15:25 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 function loadLoginForm () {
     var loginHtml = `
-        <h3>Login/Register to continue</h3>
+        <h3>Login/Register to unlock awesome features</h3>
         <input type="text" id="username" placeholder="username" />
         <input type="password" id="password" />
         <br/><br/>
@@ -8,8 +8,8 @@ function loadLoginForm () {
         <input type="submit" id="register_btn" value="Register" />
         `;
     document.getElementById('login_area').innerHTML = loginHtml;
-}
-     // Submit username/password to login
+    
+    // Submit username/password to login
     var submit = document.getElementById('login_btn');
     submit.onclick = function () {
         // Create a request object
@@ -31,47 +31,23 @@ function loadLoginForm () {
                   submit.value = 'Login';
               }
               loadLogin();
-          } 
+          }  
           // Not done yet
-        }
         };
-    
-var submit = document.getElementById('submit_btn');
-submit.onclick = function(){
-    //make a req to the server and send the name
-    //capture a list of names and render it as a list
-    //create a req obj
-    var request = new XMLHttpRequest();
-    
-    //capture the response and store it in a variable
-    request.onreadystatechange = function(){
-        if(request.readyState===XMLHttpRequest.DONE){
-            //take some action
-            if(request.status===200){
-            alert('Logged in!!');
-            }else if(request.status===403){
-                alert('Username/password is invalid');
-            }else if(request.status===500){
-                alert('Something went wrong');
-                
-            }
         
-    }
-    
-
-        //not done yet
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+        
     };
-    //make req
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    console.log(username);
-    console.log(password);
-    request.open('POST','http://haricmdk.imad.hasura-app.io/login',true);
-    request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({username:username,password:password}));
-};
-
-var register = document.getElementById('register_btn');
+    
+    var register = document.getElementById('register_btn');
     register.onclick = function () {
         // Create a request object
         var request = new XMLHttpRequest();
@@ -90,15 +66,19 @@ var register = document.getElementById('register_btn');
           }
         };
         
-    //make req
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    console.log(username);
-    console.log(password);
-    request.open('POST','http://haricmdk.imad.hasura-app.io/create-user',true);
-    request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({username:username,password:password}));
-};
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        register.value = 'Registering...';
+    
+    };
+}
+
 function loadLoggedInUser (username) {
     var loginArea = document.getElementById('login_area');
     loginArea.innerHTML = `
@@ -138,10 +118,10 @@ function loadArticles () {
                     <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
                     (${articleData[i].date.split('T')[0]})</li>`;
                 }
-                content += "</ul>";
+                content += "</ul>"
                 articles.innerHTML = content;
             } else {
-                articles.innerHTML('Oops! Could not load all articles!');
+                articles.innerHTML('Oops! Could not load all articles!')
             }
         }
     };
