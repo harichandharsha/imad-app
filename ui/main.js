1be5f991a1e767d291657ff843e80ff03976d0b9
harichandharsha <30806647+harichandharsha@users.noreply.github.com>
2017-08-25 07:52:14 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,5 +33,5 @@ submit.onclick = function(){
     console.log(password);
     request.open('POST','http://haricmdk.imad.hasura-app.io/login',true);
     request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON,stringify({username:username,password:password}));
+    request.send(JSON.stringify({username:username,password:password}));
 };
