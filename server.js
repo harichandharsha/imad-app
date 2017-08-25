00c14f02803a282b1110521c076af2c1d87361f1
harichandharsha <30806647+harichandharsha@users.noreply.github.com>
2017-08-25 08:48:16 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ app.post('/login',function(req,res){
    });
 });
 app.get('/check-login',function(req,res){
-   if(req.session&&req.session,auth&&req.session.auth.userId){
+   if(req.session&&req.session.auth&&req.session.auth.userId){
        res.send('Yoy are logged in:'+req.session.auth.userId.toString());
    }else{
        res.send('You are not logged in');
