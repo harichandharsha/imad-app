b9c2bc1e7aa5b76e2b31c83bcd41e93a0fdb19a2
harichandharsha <30806647+harichandharsha@users.noreply.github.com>
2017-08-26 02:51:36 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,7 +79,6 @@ app.get('/hash/:input', function(req, res) {
    res.send(hashedString);
 });
 
-
 app.post('/create-user', function (req, res) {
    // username, password
    // {"username": "tanmai", "password": "password"}
@@ -129,6 +128,7 @@ app.post('/login', function (req, res) {
       }
    });
 });
+
 app.get('/check-login', function (req, res) {
    if (req.session && req.session.auth && req.session.auth.userId) {
        // Load the user object
@@ -148,8 +148,9 @@ app.get('/logout', function (req, res) {
    delete req.session.auth;
    res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
 });
-    
+
 var pool = new Pool(config);
+
 app.get('/get-articles', function (req, res) {
    // make a select request
    // return a response with the results
@@ -205,53 +206,28 @@ app.post('/submit-comment/:articleName', function (req, res) {
     }
 });
 
-
-var counter = 0;
-app.get('/counter',function(req,res){
-   counter=counter +1;
-   res.send(counter.toString());
-});
-var names=[];
-app.get('/submit-name',function(req,res){//url:/submit-name?home=xxxx
-   //get the name from the request
-   var name=req.query.name;
-   names.push(name);
-   //JSON:JavaScript Object Notation
-   res.send(JSON.stringify(names));
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-app.get('/articles/:articleName',function (req,res){
-    //articleName == Article one
-    //articles[articleName] == {} content object for article one
-    //SELECT * FROM article WHERE title = 'aticle-one' 
-    pool.query("SELECT * FROM article WHERE title = $1",[req.params.articleName], function(err,result){
-    if(err){
+app.get('/articles/:articleName', function (req, res) {
+  // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
+  pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
+    if (err) {
         res.status(500).send(err.toString());
-    }else{
-        if(result.rows.length===0){
+    } else {
+        if (result.rows.length === 0) {
             res.status(404).send('Article not found');
-        }else{
+        } else {
             var articleData = result.rows[0];
-            res.send(createTemplate(articleData));      
+            res.send(createTemplate(articleData));
         }
     }
-    });
-    });
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+  });
 });
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+
+app.get('/ui/:fileName', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
 });
 
 
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
